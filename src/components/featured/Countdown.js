13b580e1cda5ae2fb8c126e72c2054fcd85d875c
GideonOdiokine <gidysmartodiokineh>
2021-06-30 04:04:08 +0100

    added a react slick library

--- src/components/featured/Countdown.js
+++ src/components/featured/Countdown.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/fade";
 
 export default class Countdown extends Component {
   render() {
     return (
       <div className="countdown_wrapper">
-        <div className="countdown_top">Event starts in</div>
-        <div className="countdown_bottom">
+        <Fade left>
+          <div className="countdown_top">Event starts in</div>
+          <div className="countdown_bottom">
             <div className="countdown_item">
-                 <div className="countdown_time">
-                     23
-                 </div>
-                 <div className="countdown_tag">
-                     Days
-                 </div>
+              <div className="countdown_time">23</div>
+              <div className="countdown_tag">Days</div>
             </div>
-        </div>
+            <div className="countdown_item">
+              <div className="countdown_time">2</div>
+              <div className="countdown_tag">Hs</div>
+            </div>
+            <div className="countdown_item">
+              <div className="countdown_time">20</div>
+              <div className="countdown_tag">Min</div>
+            </div>
+
+            <div className="countdown_item">
+              <div className="countdown_time">40</div>
+              <div className="countdown_tag">Sec</div>
+            </div>
+          </div>
+        </Fade>
       </div>
     );
   }
